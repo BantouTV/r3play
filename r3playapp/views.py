c61333eafabff7e9eda8dd1044dcca29aa1ee5df
Leandro Costa <leandrocostaweb@gmail.com>
2012-01-13 16:50:28 -0200

    Iniciando o template da home e vinculando os estáticos.

--- r3playapp/views.py
+++ r3playapp/views.py
@@ -1,1 +1,16 @@
-# Create your views here.
+from r3play.r3playapp.models import Filmes
+from django.template import RequestContext
+from django.shortcuts import render_to_response, get_object_or_404
+
+def index(request):
+    return render_to_response('index.html')
+    
+def home(request):
+    lista_ultimos_filmes = Filmes.objects.all()[:15]
+    return render_to_response('home.html', {'lista_ultimos_filmes': lista_ultimos_filmes}, context_instance=RequestContext(request))
+    
+
+def filme(request, filme_id):
+    filme = get_object_or_404(Filmes, id=filme_id)
+    return render_to_response('filme.html', {'filme': filme})
+    
--- urls.py
+++ urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
@@ -9,9 +8,10 @@ urlpatterns = patterns('',
     # url(r'^$', 'r3play.views.home', name='home'),
     # url(r'^r3play/', include('r3play.r3playapp.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    
+    url(r'^$', 'r3play.r3playapp.views.index', name='index'),
+    url(r'^home/$', 'r3play.r3playapp.views.home', name='home'),
+    url(r'^filmes/(?P<filme_id>\d+)/$', 'r3play.r3playapp.views.filme', name='filme'),
 )
