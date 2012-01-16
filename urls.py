ceec6868dc7fca5dbe2381216904ae937af1ec5e
Leandro Costa <leandrocostaweb@gmail.com>
2012-01-16 21:24:28 -0200

    Alterando a url da administração do projeto.

--- urls.py
+++ urls.py
@@ -8,8 +8,8 @@ urlpatterns = patterns('',
     # url(r'^$', 'r3play.views.home', name='home'),
     # url(r'^r3play/', include('r3play.r3playapp.urls')),
 
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^adm1n/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^adm1n/', include(admin.site.urls)),
     
     url(r'^$', 'r3play.r3playapp.views.index', name='index'),
     url(r'^home/$', 'r3play.r3playapp.views.home', name='home'),
