029158c385d9b730846bee753f0797859bc8e050
Leandro Costa <leandrocostaweb@gmail.com>
2012-02-02 14:54:19 -0200

    Correções do settings para produção.

--- settings.py
+++ settings.py
@@ -73,7 +73,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/home/ec2-user/r3play/r3playapp/static"
+    "/home/ec2-user/r3play/r3playapp/static",
 )
 
 # List of finder classes that know how to find static files in
