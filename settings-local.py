bfd13565d881461a11d5aecc3349676e9f6259f7
Leandro Costa <leandrocostaweb@gmail.com>
2012-02-02 14:50:07 -0200

    Renomeando os arquivos de settigns, facilitando o collectstatic no servidor.

--- settings-local.py
+++ settings-local.py
@@ -1,5 +1,7 @@
 # Django settings for r3play project.
 
+#import os
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -45,35 +47,34 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/usr/share/nginx/html/static/media/'
+MEDIA_ROOT = '/Users/leandrocosta/Projetos/r3play/r3playapp/static/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = 'http://r3play.com/static/media/' 
+MEDIA_URL = 'http://localhost:8000/static/' 
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/usr/share/nginx/html/static/'
+STATIC_ROOT = '/Users/leandrocosta/Projetos/r3play/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
 # arquivos estaticos CSS, JS, imagens enviadas via upload
-STATIC_URL = 'http://r3play.com/static/'
+STATIC_URL = '/static/'
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
 # Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = 'http://r3play.com/static/admin/'
+ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/home/ec2-user/r3play/r3playapp/static"
 )
 
 # List of finder classes that know how to find static files in
